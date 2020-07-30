71a546883a483e6b4e31962e8efd22607152e6fd
Andrea Colombo <andrea.colombo4@studio.unibo.it>
2020-07-30 16:52:18 +0200

    Improve example

--- image-picker/src/main/java/eu/acolombo/imagepicker/ActivityResultContracts.kt
+++ image-picker/src/main/java/eu/acolombo/imagepicker/ActivityResultContracts.kt
@@ -1,29 +1,54 @@
 package eu.acolombo.imagepicker
 
+import android.app.Activity.RESULT_OK
+import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.provider.MediaStore
 import android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+import androidx.activity.ComponentActivity
+import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContract
 
 object ActivityResultContracts {
 
-    class ChooseContracts : ActivityResultContract<List<ActivityResultContract<*,*>>, Intent>(){
-
-        override fun parseResult(resultCode: Int, intent: Intent?) = intent
+    class Choose : ActivityResultContract<List<ActivityResultContract<*, *>>, ActivityResult>() {
 
         override fun createIntent(
             context: Context,
-            input: List<ActivityResultContract<*,*>>
+            input: List<ActivityResultContract<*, *>>
         ) = Intent(Intent.ACTION_CHOOSER).apply {
             putExtra(Intent.EXTRA_INTENT, input.first().createIntent(context, null))
-            putExtra(Intent.EXTRA_INITIAL_INTENTS, input.drop(1).map { it.createIntent(context, null) }.toTypedArray())
+            putExtra(Intent.EXTRA_INITIAL_INTENTS, input.drop(1).map {
+                it.createIntent(context, null)
+            }.toTypedArray())
         }
+
+        override fun parseResult(resultCode: Int, intent: Intent?): ActivityResult =
+            ActivityResult(resultCode, intent)
 
     }
 
-    class PickImage : ActivityResultContract<Unit, Uri>(){
+    // Same as ActivityResultContracts.TakePicture but without the need to pass the uri when getting the intent
+    class TakePhoto(activity: ComponentActivity) : ActivityResultContract<Unit, Uri?>() {
+
+        var uri: Uri? = activity.contentResolver.insert(EXTERNAL_CONTENT_URI, ContentValues())
+
+        override fun createIntent(
+            context: Context,
+            input: Unit?
+        ) = Intent(MediaStore.ACTION_IMAGE_CAPTURE).putExtra(MediaStore.EXTRA_OUTPUT, uri)
+
+        override fun parseResult(resultCode: Int, intent: Intent?): Uri? =
+            if (resultCode == RESULT_OK) uri else {
+                uri = null
+                null
+            }
+
+    }
+
+    class PickImage : ActivityResultContract<Unit, Uri>() {
 
         override fun createIntent(
             context: Context,
--- image-picker/src/main/java/eu/acolombo/imagepicker/ImagePicker.kt
+++ image-picker/src/main/java/eu/acolombo/imagepicker/ImagePicker.kt
@@ -1,23 +1,42 @@
 package eu.acolombo.imagepicker
 
+import android.app.Activity
+import android.content.ContentValues
 import android.graphics.Bitmap
+import android.net.Uri
+import android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI
 import androidx.activity.ComponentActivity
-import androidx.activity.result.contract.ActivityResultContract
-import androidx.activity.result.contract.ActivityResultContracts.TakePicturePreview
-import eu.acolombo.imagepicker.ActivityResultContracts.ChooseContracts
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
+import eu.acolombo.imagepicker.ActivityResultContracts.Choose
 import eu.acolombo.imagepicker.ActivityResultContracts.PickImage
+import eu.acolombo.imagepicker.ActivityResultContracts.TakePhoto
 
-fun ComponentActivity.pickImage(
-    intents: List<ActivityResultContract<*, *>> = listOf(TakePicturePreview(), PickImage()),
-    onPick: (Any?) -> Unit
-) {
+fun ComponentActivity.pickImage(onResult: (Any?) -> Unit) {
+    val photo = TakePhoto(this)
+    val image = PickImage()
 
-    registerForActivityResult(ChooseContracts()) { intent ->
-        onPick(
-            intent?.clipData?.items?.map { it.uri.toString() }?.toList()?.first()
-                ?: intent?.dataString
-                ?: intent?.extras?.get("data") as? Bitmap
+    registerForActivityResult(Choose()) { result ->
+        onResult(
+            if (result.resultCode == Activity.RESULT_OK) result.data.let { intent ->
+                intent?.clipData?.items?.map { it.uri.toString() }?.toList()?.first()
+                    ?: intent?.dataString
+                    ?: intent?.extras?.get("data") as? Bitmap
+                    ?: photo.uri
+                    ?: return@registerForActivityResult
+            } else return@registerForActivityResult
         )
-    }.launch(intents)
-
+    }.launch(listOf(photo, image))
 }
+
+fun Fragment.pickImage(onPick: (Any?) -> Unit) = activity?.pickImage(onPick)
+
+fun ComponentActivity.takePicture(onResult: (Uri) -> Unit) {
+    contentResolver.insert(EXTERNAL_CONTENT_URI, ContentValues())?.let { uri ->
+        registerForActivityResult(ActivityResultContracts.TakePicture()) { ok ->
+            if (ok) onResult(uri)
+        }.launch(uri)
+    }
+}
+
+fun Fragment.takePicture(onTake: (Uri) -> Unit) = activity?.takePicture(onTake)
